6898242854061783df234fc9924db4648ab8b605
Jaswanthnaru <narujaswanth@gmail.com>
2026-04-26 10:29:32 +0530

    Time: 15 ms (42.23%) | Memory: 49.4 MB (25.81%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
+                    q.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()){
+
+            int[] cell = q.poll();
+            int r = cell[0];
+            int c = cell[1];
+
+            for(int[] d : dir){
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && !visited[nr][nc]){
+
+                    mat[nr][nc] = mat[r][c] + 1;
+
+                    visited[nr][nc] = true;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return mat;
+    }
+}
